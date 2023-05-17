719dac36a349206eebe6e6aed9efcfb242ef20f4
Mahmoud Ali <105460542+mahmoudAliaboElhassan@users.noreply.github.com>
2023-05-18 02:55:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 let date = new Date();
 let element = document.querySelector(".date");
 let skills = document.querySelectorAll(".bar span");
+let header = document.querySelector("#navbar");
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("nav ul li a");
+let menu = document.querySelector("#menu-icon");
+let nav = document.querySelector("nav ul");
 element.innerHTML = `&copy;<span class="name">Mahmoud</span> ${date.getFullYear()} All Right Reserved `;
-window.onscroll = function() {
-    addandremove();
-};
+window.onscroll = () => {
+    header.classList.toggle("sticky", window.scrollY > 100);
+    sections.forEach((sec) => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
 
-function addandremove() {
-    skills.forEach((sk) => {
-        if (window.scrollY >= 500) {
-            sk.classList.add("this");
-            console.log(element);
-        } else {
-            sk.classList.remove("this");
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach((link) => {
+                link.classList.remove("active");
+                console.log(link);
+                document
+                    .querySelector("nav ul li a[href*=" + id + "]")
+                    .classList.add("active");
+            });
         }
     });
-}
+    menu.classList.remove("fa-xmark");
+    nav.classList.remove("show");
+};
+menu.onclick = () => {
+    menu.classList.toggle("fa-xmark");
+    nav.classList.toggle("show");
+};
